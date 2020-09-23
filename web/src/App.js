042d263aa42fda4d7ac1cfb1e29dbdff2b2388ce
Lidia Cunha <lidiagomes121212@gmail.com>
2020-09-23 06:49:14 -0300

    Correção erro de digitação: globalStyles

--- web/src/App.js
+++ web/src/App.js
@@ -6,7 +6,7 @@ import Card_email from "./pages/Card_email/index.js";
 import { GlobalStyle } from "./styles/GlobalStyles";
 
 import Routes from "./routes";
-import {GlobalStyles} from "./styles/globalStyles";
+import {GlobalStyles} from "./styles/GlobalStyles";
 
 function App() {
   return (
