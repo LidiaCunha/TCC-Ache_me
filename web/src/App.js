6819e2223d2a17da5c68d056604f4e320535cbc6
Everson <tutinha296@gmal.com>
2020-09-23 11:45:14 -0300

    Excusão da pasta Cadastro, alteração no nome da pasta Login para Login_cadastro, responsividade das telas de login e cadastro

--- web/src/App.js
+++ web/src/App.js
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
-import Login from "./pages/Login/index.js";
-import Cadastro from "./pages/Cadastro/index.js";
+import Login from "./pages/Login_cadastro/index.js";
 import Card_email from "./pages/Card_email/index.js";
 
 import { GlobalStyle } from "./styles/GlobalStyles";
