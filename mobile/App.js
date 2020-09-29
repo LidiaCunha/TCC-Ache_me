acc1c5c5961a70911f75e4e8faa215d11a2f69c2
Erick Matheus <erick.matheusrs1003@gmail.com>
2020-09-29 12:07:51 -0300

    adicionando navegação entre telas

--- mobile/App.js
+++ mobile/App.js
@@ -1,14 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import Login from './src/pages/login/index';
-import CadastroPessoal from './src/pages/cadastro_dados_pessoais/index';
+import Routes from './src/routes';
+import { StatusBar } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
 
 export default function App() {
   return (
-    <>
-    <CadastroPessoal/>
-    {/* <Login/> */}
-    </>
+    <NavigationContainer>
+      <StatusBar barStyle="light-content"/>
+      <Routes/>
+    </NavigationContainer>
   );
 }
