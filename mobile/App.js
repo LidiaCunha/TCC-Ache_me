a9e750ae533757c58063a23a6cfe8ca26dbd9643
Lidia Cunha <lidiagomes121212@gmail.com>
2020-10-23 10:02:09 -0300

    Resolvendo problemas no package-lock

--- mobile/App.js
+++ mobile/App.js
@@ -3,18 +3,6 @@ import React from 'react';
 import Routes from './src/routes';
 import { NavigationContainer } from '@react-navigation/native';
 import { AuthProvider } from './src/contexts/auth';
-<<<<<<< HEAD
-import Chat from "./src/pages/chat/index"
-import Contatos from "./src/pages/chat/listaContatos"
-
-const App = () => {
-  return (
-    // <Contatos/>
-    <NavigationContainer>
-    <AuthProvider>
-    <Routes/>
-    </AuthProvider>
-=======
 import { ConversationProvider } from './src/contexts/ConversationProvider';
 import { SocketProvider } from './src/contexts/socketProvider';
 
@@ -30,7 +18,6 @@ const App = () => {
           </ConversationProvider>
         </SocketProvider>
       </AuthProvider>
->>>>>>> 0b8ae89128556aa8c716638a2d0623649f35ab0b
     </NavigationContainer>
   );
 }
