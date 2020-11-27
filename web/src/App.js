51fdc20eef301fb645d49c151f4d9880cf196ebb
IuriSampaio <iurisampaio18@gmail.com>
2020-11-27 11:31:28 -0300

    adicionando testes com selenium

--- web/src/App.js
+++ web/src/App.js
@@ -1,24 +1,12 @@
 
 import React from "react";
 import { GlobalStyles } from "./styles/GlobalStyles";
-<<<<<<< HEAD
-import Post from "./pages/user_info/modalPostagem";
-=======
-import Post from "./pages/post/index";
-
->>>>>>> bb9a922220e7b33c85610dcb279368de4175dcb4
 import Routes from "./routes";
-
-import Everson from "./pages/user_info/index";
 
 function App() {  
   return (
     <>
-<<<<<<< HEAD
-      <Everson/>
-=======
-      <Post />
->>>>>>> bb9a922220e7b33c85610dcb279368de4175dcb4
+      <Routes />
       <GlobalStyles/>
     </>
   );
