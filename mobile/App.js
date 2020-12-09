683556302b87500509324eed71630d503324c995
Erick Matheus <erick.matheusrs1003@gmail.com>
2020-12-09 10:50:21 -0300

    criar postagem mobile e arrumando erros

--- mobile/App.js
+++ mobile/App.js
@@ -23,8 +23,8 @@
 
 import React from 'react';
 
-// import Routes from './src/routes';
-import Routes from './src/routes/app.routes';
+import Routes from './src/routes';
+// import Routes from './src/routes/app.routes';
 import { NavigationContainer } from '@react-navigation/native';
 import { AuthProvider } from './src/contexts/auth';
 import { ConversationProvider } from './src/contexts/ConversationProvider';
