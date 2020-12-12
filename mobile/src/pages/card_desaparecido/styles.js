b70cf76a5f52aeb5abd1e625d3a43ea45071ca6f
Erick Matheus <erick.matheusrs1003@gmail.com>
2020-12-11 21:05:52 -0300

    arrumando erro

--- mobile/src/pages/card_desaparecido/styles.js
+++ mobile/src/pages/card_desaparecido/styles.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import styled from "styled-components/native";
 
 export const Text = styled.Text`
@@ -7,14 +6,12 @@ export const Text = styled.Text`
     color: #fff;
     font-size: 14px; 
 `;
-
 export const Container = styled.ScrollView`
     height: auto;
     width: 100%;
     background: #292929;
-    paddingTop: 20px;
+    padding-top: 20px;
 `;
-
 export const ContainerUser = styled.View`
     height: 75px;
     width: 100%;
@@ -24,37 +21,28 @@ export const ContainerUser = styled.View`
     align-items: center;
     align-content: center;
     flex-direction: row;
-    paddingLeft: 10px;
-    marginBottom: 10px;
+    padding-left: 10px;
+    margin-bottom: 10px;
 `;
-
 export const ContainerUserImg = styled.Image`
     height: 45px;
     width: 45px;
     background: orange;
-    borderRadius: 50px;
-    marginRight: 10px;
+    border-radius: 50px;
+    margin-right: 10px;
 `;
-
 export const ContainerUserNameData = styled.View`
     height: auto;
     width: auto;    
 `;
-
 export const Name = styled.Text`
     color: #fff;
     font-size: 16px;
 `;
-
 export const Data = styled.Text`
     color: rgba(255 , 255 , 255 , .6);
     font-size: 11px;
 `;
-
-
-
-
-
 export const ContainerImg = styled.Image`
     height: 240px;
     width: 100%;
@@ -63,7 +51,6 @@ export const ContainerImg = styled.Image`
     border-bottom-left-radius: 20px;
     
 `;
-
 export const ContainerBasicInfo = styled.View`
     height: 50px;
     width: 100%;
@@ -76,7 +63,6 @@ export const ContainerBasicInfo = styled.View`
     padding-left: 10px;
     padding-right: 10px;
 `;
-
 export const BasicInfos = styled.Text`
     height: 60%;
     width: auto;
@@ -93,7 +79,6 @@ export const BasicInfos = styled.Text`
     border-color: rgba(255,255,255,.5);
     border-right-width: 1px;
 `;
-
 export const BasicInfos2 = styled.Text`
     height: 60%;
     width: auto;
@@ -107,13 +92,11 @@ export const BasicInfos2 = styled.Text`
     font-size: 13px;
     color: #fff;
 `;
-
 export const BasicInfosImg = styled.Image`
     height: 15px;
     width: 15px;
     margin-left: 8px;
 `;
-
 export const ContainerText = styled.View`
     height: auto;
     min-height: 5px;
@@ -123,16 +106,14 @@ export const ContainerText = styled.View`
     padding-right: 20px;
     overflow: hidden;
 `;
-
 export const ContainerCharacter = styled.View`
     height: auto;
     width: 100%;
     background: #292929;
     padding: 20px;
-    paddingTop: 10px;
+    padding-top: 10px;
     overflow: hidden;
 `;
-
 export const TitleCharacter = styled.Text`
     height: auto;
     width: auto;
@@ -143,23 +124,20 @@ export const TitleCharacter = styled.Text`
     text-transform: uppercase;
     font-weight: bold;
 `;
-
 export const CardCharacter = styled.ScrollView`
     height: auto;
     min-height: 10px;
     display: flex;
     flex-direction: row;
 `;
-
 export const CardColor = styled.Text`
     height: auto;
     width: auto;
     padding: 10px;
     color: #fff;
-    borderRadius: 5px;
+    border-radius: 5px;
     margin: 5px;
 `;
-
 export const Comment = styled.View`
     height: 80px;
     width: 100%;
@@ -169,14 +147,12 @@ export const Comment = styled.View`
     flex-direction: row;
     background: #292929;
 `;
-
 export const PersonImage = styled.Image`
     height: 40px;
     width: 40px;
     border-radius: 50px;
     margin-right: 10px;
 `;
-
 export const Input = styled.TextInput`
     height: 45px;
     width: 260px;
@@ -187,7 +163,6 @@ export const Input = styled.TextInput`
     border-bottom-right-radius: 0;
     padding-left: 20px;
 `;
-
 export const BtnSend = styled.View`
     height: 45px;
     width: 43px;
@@ -198,274 +173,11 @@ export const BtnSend = styled.View`
     padding-top: 6px;
     
 `;
-
 export const BtnSendImg = styled.View`
     height: 35px;
     width: 35px;
     border-radius: 50px;
     background: #E33336;
-    justifyContent: center;
-    alignItems: center;
-        
-
-`;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-=======
-import styled from "styled-components/native";
-
-export const Text = styled.Text`
-    height: auto;
-    width: auto;
-    color: #fff;
-    font-size: 14px; 
-`;
-
-export const Container = styled.ScrollView`
-    height: auto;
-    width: 100%;
-    background: #292929;
-    paddingTop: 20px;
-`;
-
-export const ContainerUser = styled.View`
-    height: 75px;
-    width: 100%;
-    background: #303030;
-    display: flex;
-    justify-content: flex-start;
-    align-items: center;
-    align-content: center;
-    flex-direction: row;
-    paddingLeft: 10px;
-    marginBottom: 10px;
-`;
-
-export const ContainerUserImg = styled.Image`
-    height: 45px;
-    width: 45px;
-    background: orange;
-    borderRadius: 50px;
-    marginRight: 10px;
-`;
-
-export const ContainerUserNameData = styled.View`
-    height: auto;
-    width: auto;    
-`;
-
-export const Name = styled.Text`
-    color: #fff;
-    font-size: 16px;
-`;
-
-export const Data = styled.Text`
-    color: rgba(255 , 255 , 255 , .6);
-    font-size: 11px;
-`;
-
-
-
-
-
-export const ContainerImg = styled.Image`
-    height: 240px;
-    width: 100%;
-    background: chartreuse;
-    border-top-right-radius: 20px;
-    border-bottom-left-radius: 20px;
-    
-`;
-
-export const ContainerBasicInfo = styled.View`
-    height: 50px;
-    width: 100%;
-    background: #292929;
-    display: flex;
     justify-content: center;
     align-items: center;
-    align-content: center;
-    flex-direction: row;
-    padding-left: 10px;
-    padding-right: 10px;
 `;
-
-export const BasicInfos = styled.Text`
-    height: 60%;
-    width: auto;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    align-content: center;
-    padding-left: 5px;
-    padding-right: 5px;
-    padding-top: 5px;
-    font-size: 12px;
-    color: #fff;
-    border-style: solid;
-    border-color: rgba(255,255,255,.5);
-    border-right-width: 1px;
-`;
-
-export const BasicInfos2 = styled.Text`
-    height: 60%;
-    width: auto;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    align-content: center;
-    padding-left: 5px;
-    padding-right: 5px;
-    padding-top: 5px;
-    font-size: 13px;
-    color: #fff;
-`;
-
-export const BasicInfosImg = styled.Image`
-    height: 15px;
-    width: 15px;
-    margin-left: 8px;
-`;
-
-export const ContainerText = styled.View`
-    height: auto;
-    min-height: 5px;
-    width: 100%;
-    background: #292929;
-    padding-left: 20px;
-    padding-right: 20px;
-    overflow: hidden;
-`;
-
-export const ContainerCharacter = styled.View`
-    height: auto;
-    width: 100%;
-    background: #292929;
-    padding: 20px;
-    paddingTop: 10px;
-    overflow: hidden;
-`;
-
-export const TitleCharacter = styled.Text`
-    height: auto;
-    width: auto;
-    color: #fff;
-    font-size: 16px;
-    margin-bottom: 10px;
-    margin-top: 5px;
-    text-transform: uppercase;
-    font-weight: bold;
-`;
-
-export const CardCharacter = styled.ScrollView`
-    height: auto;
-    min-height: 10px;
-    display: flex;
-    flex-direction: row;
-`;
-
-export const CardColor = styled.Text`
-    height: auto;
-    width: auto;
-    padding: 10px;
-    color: #fff;
-    borderRadius: 5px;
-    margin: 5px;
-`;
-
-export const Comment = styled.View`
-    height: 80px;
-    width: 100%;
-    background-color: red;
-    display: flex;
-    justify-content: center;
-    flex-direction: row;
-    background: #292929;
-`;
-
-export const PersonImage = styled.Image`
-    height: 40px;
-    width: 40px;
-    border-radius: 50px;
-    margin-right: 10px;
-`;
-
-export const Input = styled.TextInput`
-    height: 45px;
-    width: 260px;
-    border: none;
-    border-radius: 40px;
-    background: #fff;
-    border-top-right-radius: 0;
-    border-bottom-right-radius: 0;
-    padding-left: 20px;
-`;
-
-export const BtnSend = styled.View`
-    height: 45px;
-    width: 43px;
-    border-radius: 50px;
-    background: #fff;
-    border-top-left-radius: 0;
-    border-bottom-left-radius: 0;
-    padding-top: 6px;
-    
-`;
-
-export const BtnSendImg = styled.View`
-    height: 35px;
-    width: 35px;
-    border-radius: 50px;
-    background: #E33336;
-    justifyContent: center;
-    alignItems: center;
-        
-
-`;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
->>>>>>> be602db92f28272861b4a645c326e98488f345f8
