90afe6dc8fab6caaf01b517067affcb41f8dd619
Iuri Sampaio <iurisampaio18@gmail.com>
2020-12-14 05:39:46 -0300

    adicionando comentarios ao mobile

--- backend/source/config/config.js
+++ backend/source/config/config.js
@@ -1,21 +1,21 @@
 module.exports = {
-	url: process.env.DATABASE_URL,
-	config:{
-		dialect :  "postgres",
-		logging :  console.log,
-		define  :  {
-			timestamp  : true,
-			underscored: true,
-		},
-	}
+//	url: process.env.DATABASE_URL,
+//	config:{
+//		dialect :  "postgres",
+//		logging :  console.log,
+//		define  :  {
+//			timestamp  : true,
+//			underscored: true,
+//		},
+//	}
 	
-	// dialect :  "mysql",
-	// host    :  "localhost",
-	// username:  "root",
-	// password:  "bcd127",
-	// database:  "TCC_SENAI",
-	// define  : {
-	// 	timestamp  : true,
-	// 	underscored: true,
-	// },
+	 dialect :  "mysql",
+	 host    :  "localhost",
+	 username:  "root",
+	 password:  "bcd127",
+	 database:  "TCC_SENAI",
+	 define  : {
+	 	timestamp  : true,
+	 	underscored: true,
+	 },
 }
